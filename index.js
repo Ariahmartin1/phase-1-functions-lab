18f1d3dfa7a644cdcffc16364c7802b428ccb8ef
zachariah martin <zachariahsmartin@gmail.com>
2022-09-03 16:13:00 -0400

    commit tree a7f1d26fd982b47fcbed16be4e7c246bd07de4d9

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blocksPickUp) {
+    return (blocksPickUp > 42) ? (blocksPickUp - 42) : (42 - blocksPickUp)
+}
+
+function distanceFromHqInFeet(feetPickUp) {
+   return distanceFromHqInBlocks(feetPickUp) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return ( start >= destination) ? ((start - destination) * 264) : ((destination - start) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && (distanceTravelledInFeet(start, destination) <= 2000)) {
+        return (distanceTravelledInFeet(start, destination)- 400) * .02
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000 && (distanceTravelledInFeet(start, destination) <= 2500)) {
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far"
+    }
+  }
